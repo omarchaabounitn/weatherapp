5ece08d666eb318a2c5c154b764a6b353b9af4df
Omar Chaabouni <omar.chaabouni@devoteam.com>
2019-11-01 15:31:59 +0100

    add provider

---
+++ WeatherApp/data/api/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  WeatherApp
+//
+//  Created by Omar Chaabouni on 29/10/2019.
+//  Copyright © 2019 Omar Chaabouni. All rights reserved.
+//
+
+import Foundation
+
+enum Constant: String {
+    case apiURL = "https://www.infoclimat.fr/public-api/gfs/json"
+    case apiAuth = "AxkHEFUrXH4HKltsD3kBKFU9BzILfQIlUCwEZw9qBXhUPwJjD29dOwJsAH0AL1ZgVntUNwoxBjYBagd%2FXS8HZgNpB2tVPlw7B2hbPg8gASpVewdmCysCJVAyBGQPZAV4VDYCYA9sXSECagBjAC5WY1ZnVDUKKgYhAWMHZV03B2YDYwdlVTVcNwdtWzAPIAEqVWMHMwtmAjJQNgQ2DzAFYVQzAmEPPl06AjgAZgAuVmdWZFQwCjEGOwFgB2BdNwd7A38HGlVFXCMHKFt7D2oBc1V7BzILagJu"
+    case apiKey = "d3c35ad5c19b3a2d627f4a39392d1b56"
+}
